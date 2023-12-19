93b6f7358751d1864f8b907e0c5b23d9cab7c724
Caitlin Turnidge <caitlinturnidge@Caitlins-MacBook-Pro.local>
2023-12-19 09:49:47 +0000

    remove last test

--- test_load.py
+++ test_load.py
@@ -35,18 +35,3 @@ def test_upload_waterings_correct_calls_empty_data(mock_get_waterings_csv):
     upload_waterings(conn)
     assert mock_execute.call_count == 3
     assert mock_commit.call_count == 0
-
-
-@patch("load.get_recordings_csv")
-@patch("load.sql.text")
-def test_upload_recordings_exception_handling(mock_sql_text, mock_get_recordings_csv):
-    """Test that rollback is called when an exception occurs."""
-    mock_get_recordings_csv.return_value = []
-    conn = MagicMock()
-    mock_execute = conn.execute
-    mock_execute.side_effect = Exception("Simulated error: Rollback failed")
-    with pytest.raises(Exception, match="Simulated error: Rollback failed"):
-        upload_recordings(conn)
-
-    assert mock_execute.call_count == 2
-    mock_execute.assert_called_with(mock_sql_text("ROLLBACK;"))
