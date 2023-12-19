2c7e54721e5a2d732168297cf78ef2a26f7519b3
david-nepo <dt.nepo@gmail.com>
2023-12-19 11:24:56 +0000

    refactor and optimise

--- extract.py
+++ extract.py
@@ -4,12 +4,29 @@ import requests
 import pandas as pd
 
 BASE_URL = 'https://data-eng-plants-api.herokuapp.com/plants/'
+NO_OF_PLANTS = 51
 
 
-def get_plant_data(plant_id: int) -> list[dict]:
+def get_plant_data(plant_id: int) -> dict:
     """Gets plant data from API using ID."""
 
-    return requests.get(BASE_URL + str(plant_id), timeout=100).json()
+    api_data = requests.get(BASE_URL + str(plant_id), timeout=100).json()
+
+    if 'error' in api_data:
+        print(f"Error: {api_data.get('error')}")
+
+    return api_data
+
+
+def get_plant_data(plant_id: int, session: requests.Session) -> dict:
+    """Gets plant data from API using ID."""
+
+    api_data = session.get(BASE_URL + str(plant_id), timeout=100).json()
+
+    if 'error' in api_data:
+        print(f"Error: {api_data.get('error')}")
+
+    return api_data
 
 
 def get_recording_data(data: dict) -> dict:
@@ -52,34 +69,68 @@ def get_plant_names(data: dict) -> dict:
     return {key: data.get(key) for key in relevant_cols}
 
 
+def get_origin_data(data: dict) -> dict:
+    """Returns origin data as dictionary."""
+
+    if 'origin_location' in data:
+        origin = data.get('origin_location')
+
+        lat, lon = origin[0], origin[1]
+        town, country_code = origin[2], origin[3]
+
+        region = origin[-1].split('/')
+        country, capital = region[0], region[1]
+
+        origin_dict = {
+            'lat': lat, 'lon': lon,
+            'town': town, 'country_code': country_code,
+            'country': country, 'capital': capital
+        }
+
+        return origin_dict
+
+    print('No origin location data.')
+    return None
+
+
 if __name__ == "__main__":
-    recording_data_list = []
-    watering_data_list = []
+
+    recording_data_list, watering_data_list = [], []
+
+    # Static data
     botanist_data_list = []
     plant_data_list = []
-    location_data_list = []
+    origin_data_list = []
+    image_data_list = []
 
-    for plant_id in range(10):
-        data = get_plant_data(plant_id)
+    with requests.Session() as session:
+        for plant_id in range(NO_OF_PLANTS):
 
-        recording_data = get_recording_data(data)
-        watering_data = get_watering_data(data)
-        botanist_data = get_botanist_data(data)
-        plant_data = get_plant_names(data)
+            print(f'Retrieving data for plant {plant_id}')
 
-        recording_data_list.append(recording_data)
-        watering_data_list.append(watering_data)
-        botanist_data_list.append(botanist_data)
-        plant_data_list.append(plant_data)
+            data = get_plant_data(plant_id, session)
 
-        print(f'Retrieving data for plant {plant_id}')
+            recording_data = get_recording_data(data)
+            watering_data = get_watering_data(data)
+            botanist_data = get_botanist_data(data)
+            plant_data = get_plant_names(data)
+            origin_data = get_origin_data(data)
+
+            recording_data_list.append(recording_data)
+            watering_data_list.append(watering_data)
+            botanist_data_list.append(botanist_data)
+            plant_data_list.append(plant_data)
+            origin_data_list.append(origin_data)
 
     cleaned_botanist_data_list = get_unique_botanists(botanist_data_list)
+    cleaned_origin_data_list = [
+        origin for origin in origin_data_list if origin]
 
     recording_df = pd.DataFrame(recording_data_list)
     watering_df = pd.DataFrame(watering_data_list)
     botanist_df = pd.DataFrame(cleaned_botanist_data_list)
     plant_df = pd.DataFrame(plant_data_list)
+    origin_df = pd.DataFrame(cleaned_origin_data_list)
 
     recording_df['recording_taken'] = pd.to_datetime(
         recording_df['recording_taken'])
@@ -88,6 +139,8 @@ if __name__ == "__main__":
 
     recording_df.to_csv('recording_data_SAMPLE.csv', index=False)
     watering_df.to_csv('watering_data_SAMPLE.csv', index=False)
-    botanist_df.to_csv('botanist_data_SAMPLE.csv', index=False)
+    botanist_df.to_csv('botanist_data_STATIC.csv', index=False)
+    plant_df.to_csv('plant_name_data_STATIC.csv', index=False)
+    origin_df.to_csv('origin_data_STATIC.csv', index=False)
 
     print("DataFrames saved as CSV files.")
