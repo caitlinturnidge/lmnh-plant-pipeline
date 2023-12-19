25cf8ed7019194109d62bda359ba5caed9d3d592
david-nepo <dt.nepo@gmail.com>
2023-12-19 18:08:52 +0000

    short-term dashboard

--- dashboard/create_mock_data.py
+++ dashboard/create_mock_data.py
@@ -8,31 +8,83 @@ from datetime import datetime, timedelta
 
 if __name__ == "__main__":
 
-    initial_reading = {
-        'plant_id': 0,
-        'soil_moisture': 25.730933575024196,
-        'temperature': 13.157893007926564,
-        'recording_taken': '2023-12-19 11:21:43'
-    }
+    # Given readings for multiple plants
+    initial_readings = [
+        {'plant_id': 0, 'soil_moisture': 25.730933575024196,
+            'temperature': 13.157893007926564, 'recording_taken': '2023-12-19 11:21:43'},
+        {'plant_id': 1, 'soil_moisture': 23.98298531988189,
+            'temperature': 12.026341992360456, 'recording_taken': '2023-12-19 11:21:45'},
+        {'plant_id': 2, 'soil_moisture': 30.616930942675168,
+            'temperature': 9.116185308436853, 'recording_taken': '2023-12-19 11:21:45'},
+        {'plant_id': 3, 'soil_moisture': 29.019263723342945,
+            'temperature': 26.039994462840696, 'recording_taken': '2023-12-19 11:21:46'},
+        {'plant_id': 4, 'soil_moisture': 20.951649013916494,
+            'temperature': 11.321636304659222, 'recording_taken': '2023-12-19 11:21:47'},
+        {'plant_id': 5, 'soil_moisture': 30.434974296121098,
+            'temperature': 11.178615799217587, 'recording_taken': '2023-12-19 11:21:48'},
+        {'plant_id': 6, 'soil_moisture': 27.364642121408707,
+            'temperature': 10.96016882103583, 'recording_taken': '2023-12-19 11:21:48'}
+    ]
 
-    initial_time = datetime.strptime(
-        initial_reading['recording_taken'], '%Y-%m-%d %H:%M:%S')
+    # Convert the recording_taken string to a datetime object for each plant
+    for reading in initial_readings:
+        reading['recording_taken'] = datetime.strptime(
+            reading['recording_taken'], '%Y-%m-%d %H:%M:%S')
 
-    time_series = [initial_time + timedelta(minutes=i) for i in range(24 * 60)]
+    # Generate a time series for 24 hours at 1-minute intervals
+    time_series = [initial_readings[0]['recording_taken'] +
+                   timedelta(minutes=i) for i in range(24 * 60)]
 
-    # Introduce random fluctuations
-    moisture_variation = np.random.normal(
-        0, 1, len(time_series))
-    temperature_variation = np.random.normal(
-        0, 0.5, len(time_series))
+    # Create a DataFrame with the mock data for each plant
+    mock_data_list = []
+    for reading in initial_readings:
+        plant_id = reading['plant_id']
+        moisture_variation = np.random.normal(0, 1, len(time_series))
+        temperature_variation = np.random.normal(0, 0.5, len(time_series))
 
-    mock_data = pd.DataFrame({
-        'plant_id': [initial_reading['plant_id']] * len(time_series),
-        'soil_moisture': initial_reading['soil_moisture'] + moisture_variation,
-        'temperature': initial_reading['temperature'] + temperature_variation,
-        'recording_taken': time_series
-    })
+        plant_mock_data = pd.DataFrame({
+            'plant_id': [plant_id] * len(time_series),
+            'soil_moisture': reading['soil_moisture'] + moisture_variation,
+            'temperature': reading['temperature'] + temperature_variation,
+            'recording_taken': time_series
+        })
 
+        mock_data_list.append(plant_mock_data)
+
+    # Concatenate the mock data for each plant into a single DataFrame
+    mock_data = pd.concat(mock_data_list, ignore_index=True)
+
+    # Print or visualize the mock data
     print(mock_data.head())
 
-    mock_data.to_csv('mock_data.csv', index=False)
+    # Save the mock data to a CSV file if needed
+    mock_data.to_csv('mock_data_multi_plants.csv', index=False)
+
+    # initial_reading = {
+    #     'plant_id': 0,
+    #     'soil_moisture': 25.730933575024196,
+    #     'temperature': 13.157893007926564,
+    #     'recording_taken': '2023-12-19 11:21:43'
+    # }
+
+    # initial_time = datetime.strptime(
+    #     initial_reading['recording_taken'], '%Y-%m-%d %H:%M:%S')
+
+    # time_series = [initial_time + timedelta(minutes=i) for i in range(24 * 60)]
+
+    # # Introduce random fluctuations
+    # moisture_variation = np.random.normal(
+    #     0, 1, len(time_series))
+    # temperature_variation = np.random.normal(
+    #     0, 0.5, len(time_series))
+
+    # mock_data = pd.DataFrame({
+    #     'plant_id': [initial_reading['plant_id']] * len(time_series),
+    #     'soil_moisture': initial_reading['soil_moisture'] + moisture_variation,
+    #     'temperature': initial_reading['temperature'] + temperature_variation,
+    #     'recording_taken': time_series
+    # })
+
+    # print(mock_data.head())
+
+    # mock_data.to_csv('mock_data.csv', index=False)
--- dashboard/main.py
+++ dashboard/main.py
@@ -13,52 +13,20 @@ FIG_HEIGHT = int(FIG_WIDTH / 4)
 
 st.set_page_config(layout="wide")
 
-if __name__ == "__main__":
 
-    data = pd.read_csv('mock_data.csv')
-    data['recording_taken'] = pd.to_datetime(data['recording_taken'])
-    data.head()
+def get_moisture_data(data: pd.DataFrame) -> pd.DataFrame:
+    """Gets moisture data from filtered dataframe."""
 
-    # Sidebar content
-    st.sidebar.header('Plant Health Tracker')
-
-    resample_rate = st.sidebar.slider(
-        'Select sample rate (minutes)',
-        min_value=1, max_value=60, value=10, step=1)
-
-    RESAMPLE_RATE = f'{resample_rate}T'
-
-    # Time range sliders
-    total_hours = data['recording_taken'].max() - data['recording_taken'].min()
-    total_hours = total_hours.total_seconds() / 3600
-
-    selected_hours = st.sidebar.slider(
-        'Select number of hours to display',
-        min_value=1, max_value=int(total_hours), value=int(total_hours / 2), step=1
-    )
-
-    start_datetime = st.sidebar.slider(
-        'Select starting point',
-        min_value=0, max_value=int(total_hours - selected_hours), value=0, step=1
-    )
-
-    end_datetime = start_datetime + selected_hours
-
-    # Filter data based on selected time range
-    filtered_data = data[
-        (data['recording_taken'] >= data['recording_taken'].min() + timedelta(hours=start_datetime)) &
-        (data['recording_taken'] <= data['recording_taken'].min() +
-         timedelta(hours=end_datetime))
-    ]
-
-    latest_data = data.iloc[-1]
-
-    moisture_data = filtered_data.resample(
+    return data.resample(
         RESAMPLE_RATE, on='recording_taken').mean().reset_index()
-    y_min = moisture_data['soil_moisture'].min()
-    y_max = moisture_data['soil_moisture'].max()
 
-    chart = alt.Chart(moisture_data).mark_line(
+
+def get_soil_moisture_chart(chart_data: pd.DataFrame) -> alt.Chart:
+
+    y_min = chart_data['soil_moisture'].min()
+    y_max = chart_data['soil_moisture'].max()
+
+    chart = alt.Chart(chart_data).mark_line(
         interpolate='cardinal',
         tension=0.8,
         strokeWidth=LINE_WIDTH,
@@ -77,14 +45,18 @@ if __name__ == "__main__":
         # fontSize=TITLE_FONT_SIZE,
     )
 
-    st.header('Soil Moisture')
-    st.subheader(f"Latest Soil Moisture: {latest_data['soil_moisture']:.2f}%")
-    st.altair_chart(chart)
+    return chart
 
-    st.divider()
 
-    temperature_data = data.resample(
+def get_temperature_data(data: pd.DataFrame) -> pd.DataFrame:
+    """Gets moisture data from filtered dataframe."""
+
+    return data.resample(
         RESAMPLE_RATE, on='recording_taken').mean().reset_index()
+
+
+def get_temperature_chart(chart_data: pd.DataFrame) -> alt.Chart:
+
     y_min = temperature_data['temperature'].min()
     y_max = temperature_data['temperature'].max()
 
@@ -107,8 +79,69 @@ if __name__ == "__main__":
         # fontSize=TITLE_FONT_SIZE,
     )
 
+    return chart
+
+
+if __name__ == "__main__":
+
+    data_multi = pd.read_csv('mock_data_multi_plants.csv')
+    data_multi['recording_taken'] = pd.to_datetime(
+        data_multi['recording_taken'])
+    data_multi.head()
+
+    st.sidebar.header('Plant Health Tracker')
+
+    selected_plant = st.sidebar.selectbox(
+        'Select Plant', data_multi['plant_id'].unique())
+
+    plant_id = data_multi['plant_id'] == selected_plant
+    data = data_multi[plant_id]
+
+    resample_rate = st.sidebar.slider(
+        'Select sample rate (minutes)',
+        min_value=1, max_value=60, value=10, step=1)
+
+    RESAMPLE_RATE = f'{resample_rate}T'
+
+    # Time range sliders
+    total_hours = data['recording_taken'].max() - data['recording_taken'].min()
+    total_hours = total_hours.total_seconds() / 3600
+
+    selected_hours = st.sidebar.slider(
+        'Select number of hours to display',
+        min_value=1, max_value=int(total_hours), value=int(total_hours / 2), step=1
+    )
+
+    start_datetime = st.sidebar.slider(
+        'Select window start',
+        min_value=0, max_value=int(total_hours - selected_hours), value=0, step=1
+    )
+
+    end_datetime = start_datetime + selected_hours
+
+    # Filter data based on selected time range
+    filtered_data = data[
+        (data['recording_taken'] >= data['recording_taken'].min() + timedelta(hours=start_datetime)) &
+        (data['recording_taken'] <= data['recording_taken'].min() +
+         timedelta(hours=end_datetime))
+    ]
+
+    latest_data = data.iloc[-1]
+
+    moisture_data = get_moisture_data(filtered_data)
+    chart = get_soil_moisture_chart(moisture_data)
+
+    st.header('Soil Moisture')
+    st.metric('Current Soil Moisture', f"{latest_data['soil_moisture']:.2f}%")
+    st.altair_chart(chart)
+
+    st.divider()
+
+    temperature_data = get_temperature_data(data)
+    chart = get_temperature_chart(temperature_data)
+
     st.header('Temperature')
-    st.subheader(f"Latest Temperature: {latest_data['temperature']:.2f}°C")
+    st.metric('Current Temperature', f"{latest_data['temperature']:.2f}°C")
     st.altair_chart(chart)
 
     st.sidebar.image('https://perenual.com/storage/species_image/2961_ficus_elastica/small/533092219_8da73ba0d2_b.jpg',
