446c66d23c8063004fc648e1dc4a7670948535bb
Shurc1val <hughpm16@gmail.com>
2023-12-20 20:10:39 +0000

    Finished update_duties.py script (and requisite file database_functions.py

--- update_duties/database_functions.py
+++ update_duties/database_functions.py
@@ -1,34 +1,117 @@
 
+from datetime import datetime
 from dotenv import load_dotenv
 from os import environ
+import pandas as pd
 
 import sqlalchemy as db
 from sqlalchemy.engine.base import Connection
 
-def get_database_engine():
-    """Returns the database engine."""
+
+class MSSQL_Database():
+
+    def __init__(self) -> None:
+        """
+        Connects to engine, creates connection, and stores resulting objects along with the
+        database metadata as attributes of the image of the class.
+        """
+        load_dotenv()
+        try:
+            self.engine = db.create_engine(
+                f"mssql+pymssql://{environ['DB_USER']}:{environ['DB_PASSWORD']}@{environ['DB_HOST']}:{environ['DB_PORT']}/{environ['DB_NAME']}?charset=utf8"
+            )
+            self.connection = self.engine.connect()
+            self.metadata = db.MetaData(schema=environ['DB_SCHEMA'])
+        except Exception as e:
+            print(f"Error connecting to database: {e}")
+            raise e
+        
+    def commit(self):
+        """Commits db connection for changes made to persist externally."""
+        self.connection.commit()
+
+    def close(self):
+        """Closes db connection."""
+        self.connection.close()
+
+
+
+def add_duty(database: MSSQL_Database, plant_id: int, botanist_id: int):
+    """
+    Updates any duties in duty table with plant id and NULL end column to current datetime, and add a new duty for plant and botanist id.
+    """
+    
     try:
-        engine = db.create_engine(
-            f"mssql+pymssql://{environ['DB_USER']}:{environ['DB_PASSWORD']}@{environ['DB_HOST']}:{environ['DB_PORT']}/{environ['DB_NAME']}?charset=utf8"
-        )
-        return engine
+        duty_table = db.Table('duty', database.metadata, autoload_with=database.engine)
+        # Update old duty:
+        query = db.update(duty_table).values(end=datetime.now()).where((duty_table.c.plant_id == plant_id) & (duty_table.c.end == None))
+        database.connection.execute(query)
+
+        # Add new duty
+        query = db.insert(duty_table).values(plant_id=plant_id, botanist_id=botanist_id)
+        database.connection.execute(query)
+
+        database.commit()
+
     except Exception as e:
-        print(f"Error creating database engine: {e}")
         raise e
 
 
-def get_db_plant_ids(table_name: str, db_engine: db.Engine, connection: Connection, metadata,
-                    datetime_cutoff: str = LITERAL_DAY_AGO):
-    """
-    Retrieves records older than 24 hours (by attribute 'datetime') from db table with given name
-    (watering/recording).
-    """
+def get_db_plant_ids(database: MSSQL_Database) -> list:
+    """Retrieves all plant ids from the plant table in the db."""
     try:
-        table = db.Table(table_name, metadata, autoload_with=db_engine)
-        query = db.select(table).where(table.columns.datetime < datetime_cutoff)
-        response = connection.execute(query)
+        plant_table = db.Table('plant', database.metadata, autoload_with=database.engine)
+        query = db.select(plant_table)
+        response = database.connection.execute(query)
+        results = response.fetchall()
+        return [row.id for row in results]
+
+    except Exception as e:
+        raise e
+    
+
+def get_active_duties(database: MSSQL_Database) -> pd.DataFrame:
+    """Retrieves pandas df of all records in db duty table without an end date."""
+    try:
+        duty_table = db.Table('duty', database.metadata, autoload_with=database.engine)
+        query = db.select(duty_table).where(duty_table.columns.end == None)
+        response = database.connection.execute(query)
         results = response.fetchall()
         return pd.DataFrame(results)
 
     except Exception as e:
         raise e
+    
+
+def get_botanist_by_name(forename: str, surname: str, database: MSSQL_Database) -> pd.DataFrame:
+    """Retrieves pandas df of first record in db botanist table with specified fore- and surname."""
+    try:
+        botanist_table = db.Table('botanist', database.metadata, autoload_with=database.engine)
+        query = db.select(botanist_table).where(botanist_table.columns.firstname == forename and botanist_table.columns.lastname == surname)
+        response = database.connection.execute(query)
+        result = response.first()
+        return list(result)
+
+    except Exception as e:
+        raise e
+
+
+def get_table_records(table_name: str, database: MSSQL_Database) -> pd.DataFrame:
+    """Retrieves all records as pandas df from specified table in db."""
+    try:
+        table = db.Table(table_name, database.metadata, autoload_with=database.engine)
+        query = db.select(table)
+        response = database.connection.execute(query)
+        results = response.fetchall()
+        return pd.DataFrame(results)
+
+    except Exception as e:
+        raise e
+    
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    database = MSSQL_Database()
+
+    add_duty(database, 5, 10)
--- update_duties/update_duties.py
+++ update_duties/update_duties.py
@@ -1,12 +1,46 @@
-
+from dotenv import load_dotenv
+from functools import partial
+from os import environ
+import pandas as pd
 import requests
+
+import database_functions as dbf
+
 
 BASE_URL = "https://data-eng-plants-api.herokuapp.com/plants/"
 
 
-def get_plant_data(plant_id: int) -> list[dict]:
+def get_api_botanist_name_by_plant_id(plant_id: int) -> list[dict]:
     """Gets plant data from API using ID."""
-    return requests.get(BASE_URL + str(plant_id), timeout=100).json()
+    api_botanist_info = requests.get(BASE_URL + str(plant_id), timeout=100).json().get('botanist', {})
+    return api_botanist_info.get('name','').split()
+    
 
 
-def 
+def check_if_duty_exists_in_duties(plant_id: int, botanist_id: int, duties: pd.DataFrame) -> bool:
+    """Returns whether there is a duty in dataframe matching both plant and botanist id."""
+    return not duties[(duties['plant_id'] == plant_id) & (duties['botanist_id'] == botanist_id)].empty
+
+
+def cross_reference_api_and_db_duties():
+    """Checks and updates duty table in db based on current API data."""
+    database = dbf.MSSQL_Database()
+
+    plant_ids = dbf.get_db_plant_ids(database)
+    active_duties = dbf.get_active_duties(database)
+
+    check_for_existing_duty = partial(check_if_duty_exists_in_duties, duties = active_duties)
+
+    for plant_id in plant_ids:
+        botanist_name = get_api_botanist_name_by_plant_id(plant_id)
+        print(f'Checking duty for plant with id {plant_id}...')
+        if len(botanist_name) == 2:
+            botanist_id = dbf.get_botanist_by_name(botanist_name[0], botanist_name[1], database)[0]
+            if not check_for_existing_duty(plant_id, botanist_id):
+                dbf.add_duty(database, plant_id, botanist_id)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    cross_reference_api_and_db_duties()
