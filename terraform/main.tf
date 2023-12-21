d0256a04f4ccff821b31ca208edee04dc7e8d65d
david-nepo <dt.nepo@gmail.com>
2023-12-21 20:02:01 +0000

    dashboard terraform ecs taskdef and ecs

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,75 @@ provider "aws" {
   region = "eu-west-2" 
 }
 
+# Stuff that exists already
+data "aws_vpc" "c9-vpc" {
+    id = "vpc-04423dbb18410aece"
+}
+
+data "aws_ecs_cluster" "c9-cluster" {
+    cluster_name = "c9-ecs-cluster"
+}
+
+
 # Create ECR repository 
 
+resource "aws_ecr_repository" "dashboard-repo" {
+  name                 = "c9-butterflies-dashboard-terraform"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+
+# # Create task definition 
+
+resource "aws_ecs_task_definition" "dashboard-taskdef" {
+    family = "c9-butterflies-dashboard-taskdef-terraform"
+    requires_compatibilities = ["FARGATE"]
+    network_mode = "awsvpc"
+    container_definitions = jsonencode([
+    {
+      "name": "c9-butterflies-dashboard-terraform",
+      "image": "129033205317.dkr.ecr.eu-west-2.amazonaws.com/c9-butterflies-dashboard-repo:latest",
+      essential = true
+      environment = [
+        { name = "DB_NAME", value = "${var.DB_NAME}" },
+        { name = "DB_USER", value = "${var.DB_USER}" },
+        { name = "DB_PASSWORD", value = "${var.DB_PASSWORD}" },
+        { name = "DB_PORT", value = "${var.DB_PORT}" },
+        { name = "DB_HOST", value = "${var.DB_HOST}" },
+        { name = "DB_SCHEMA", value = "${var.DB_SCHEMA}" }
+      ]
+      portMappings = [
+        {
+          name          = "c9-butterflies-dashboard-8501-tcp"
+          containerPort = 8501
+          hostPort      = 8501
+          "protocol": "tcp",
+          "appProtocol": "http"
+        }
+      ]
+    }])
+    cpu = 1024
+    memory = 3072
+    execution_role_arn = "arn:aws:iam::129033205317:role/ecsTaskExecutionRole"
+
+}
+
+resource "aws_ecs_service" "dashboard-service" {
+    name = "c9-butterflies-dashboard-service-terraform"
+    cluster = data.aws_ecs_cluster.c9-cluster.id
+    task_definition = "arn:aws:ecs:eu-west-2:129033205317:task-definition/c9-butterflies-dashboard-taskdef:1"
+    desired_count = 1
+    launch_type = "FARGATE"
+    network_configuration {
+      subnets = ["subnet-0d0b16e76e68cf51b", "subnet-081c7c419697dec52", "subnet-02a00c7be52b00368"]
+      security_groups = ["sg-046ca8117d547057e"]
+      assign_public_ip = true
+    }
+}
 
 
 # Create a role for the lambda functions
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,33 @@ variable "BUCKET_NAME" {
     description = "bucket name"
     type        = string
 }
+
+variable "DB_HOST" {
+    description = "DB host"
+    type        = string
+}
+
+variable "DB_PORT" {
+    description = "DB port"
+    type        = string
+}
+
+variable "DB_NAME" {
+    description = "DB name"
+    type        = string
+}
+
+variable "DB_SCHEMA" {
+    description = "DB schema"
+    type        = string
+}
+
+variable "DB_USER" {
+    description = "DB user"
+    type        = string
+}
+
+variable "DB_PASSWORD" {
+    description = "DB password"
+    type        = string
+}
