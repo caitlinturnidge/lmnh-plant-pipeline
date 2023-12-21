ecbc802bd8312fa309e5404b7444d5313cd95e72
Jack Hayden <jackhaayden1@gmail.com>
2023-12-21 14:58:09 +0000

    Added terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,33 @@ variable "BUCKET_NAME" {
     description = "bucket name"
     type        = string
 }
+
+variable "DB_HOST" {
+    description = "DB host name"
+    type = string 
+}
+
+variable "DB_NAME" {
+    description = "Name of database"
+    type = string
+}
+
+variable "DB_PASSWORD" {
+    description = "Password to access database"
+    type = string 
+}
+
+variable "DB_USER" {
+    description = "Specific user of the database"
+    type = string 
+}
+
+variable "DB_PORT" {
+    description = "Port to access database"
+    type = number
+}
+
+variable "DB_SCHEMA" {
+    description = "Relevant database schema"
+    type = string
+}
